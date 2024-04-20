ed8ff05d7400e85de250121c81376e88356cdd83
Lowie Van Meensel <lowie366@gmail.com>
2024-04-21 01:34:16 +0200

    WidgetsFlutterBinding.ensureInitialized

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   // Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
