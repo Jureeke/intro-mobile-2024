75241007c61f8750cd49868fcb7fa1c07904565b
Lowie Van Meensel <lowie366@gmail.com>
2024-04-27 21:54:21 +0200

    add images and screens

---
+++ lib/src/screens/book_a_court_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class BookACourtScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Book a court Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
---
+++ lib/src/screens/play_open_match_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class PlayOpenMatchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Play an open match Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
